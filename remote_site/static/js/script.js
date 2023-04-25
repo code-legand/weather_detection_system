bf3e0c4f8550405f205181e343038e041d57eeb1
Shailesh_Kumar_23994 <shaileshkumar23994@gmail.com>
2023-04-25 10:38:24 +0530

    minor correction

--- remote_site/static/js/script.js
+++ remote_site/static/js/script.js
@@ -71,7 +71,7 @@ document.getElementById('device_id').addEventListener('input', (e) => {
 let myVar = setInterval(myTimer, 5000);
 function myTimer() {
   try {
-    XMLrequest.open('POST', BASE_URL + '/send/', true);
+    XMLrequest.open('POST', 'send/', true);
     XMLrequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     XMLrequest.setRequestHeader('X-CSRFToken', csrftoken);
     XMLrequest.send('device_id=' + device_id);
